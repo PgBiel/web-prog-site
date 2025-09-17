947827562bee3fb3aa253df15af8bbd36cd8294c
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-09-17 00:43:46 -0300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      # Systems supported by this flake
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      # A helper function for specifying per-system outputs
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      # Development environments
+      devShells = forAllSystems (
+        pkgs:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Add development environment packages here
+              python3
+              uv
+            ];
+          };
+        }
+      );
+
+      # Package outputs
+      packages = forAllSystems (
+        pkgs:
+        {
+          # Build this by running `nix build`, `nix build .`, or `nix build .#default`
+          # default = pkgs.hello;
+        }
+      );
+    };
+}
