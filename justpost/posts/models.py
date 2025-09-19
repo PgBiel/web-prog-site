5d4aa0cf8743dd64a1ccb7b6f313354ae8d6ea6e
LuizAugustoPapa <luiz.papa1204@gmail.com>
2025-09-19 15:47:41 -0300

    create Post model

--- justpost/posts/models.py
+++ justpost/posts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
