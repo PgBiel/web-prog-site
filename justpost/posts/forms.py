e24fc67a4f23b6e5b92829177216b5215816c664
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-09-28 11:26:26 -0300

    improve post form labels

--- justpost/posts/forms.py
+++ justpost/posts/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from posts.models import Post
 
+from django.utils.translation import gettext_lazy as _
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['titulo', 'conteudo']
+        labels = {
+            "titulo": _("Título"),
+            "conteudo": _("Conteúdo"),
+        }
