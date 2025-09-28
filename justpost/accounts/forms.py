a6dfd03e85cc858f1d0b5152562c582b29922918
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-09-28 11:17:46 -0300

    use UserCreationForm

--- justpost/accounts/forms.py
+++ justpost/accounts/forms.py
@@ -1,16 +1,17 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from posts.models import Post
 
 
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["username", "email", "password"]
-        widgets = {
-            "password": forms.PasswordInput(),
-        }
+class SignupForm(UserCreationForm):
+    # class Meta:
+    #     model = User
+    #     fields = ["username", "email", "password"]
+    #     widgets = {
+    #         "password": forms.PasswordInput(),
+    #     }
+    pass
 
 class LoginForm(AuthenticationForm):
     # class Meta:
